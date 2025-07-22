8db3243df1e89681d5dbb583d44cb77c7d5c76cc
hideaki-kawai <hideaki.kawai@thephage.life>
2025-07-22 16:33:44 +0900

    hono client追加

--- apps/api/vite.config.ts
+++ apps/api/vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import devServer from '@hono/vite-dev-server'
-import build from '@hono/vite-build'
+import { defineConfig } from "vite";
+import devServer from "@hono/vite-dev-server";
+import build from "@hono/vite-build";
 
 export default defineConfig({
   plugins: [
     build({
-      entry: './src/index.ts',
+      entry: "./src/index.ts",
     }),
     devServer({
-      entry: './src/index.ts',
+      entry: "./src/index.ts",
     }),
   ],
-})
+  server: {
+    port: 5174,
+  },
+});
